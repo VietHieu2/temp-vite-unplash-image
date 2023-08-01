0130c5ce43c0f2c6c1827707a9b59b8ec60fc81e
Hieu Van <viethieu972@gmail.com>
2023-08-01 11:28:45 +0700

    first-commit

---
+++ src/context.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+const getInittialDarkMode = () => {
+  const preferDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  const storedDarkMode = localStorage.getItem("darkTheme") === "true";
+  return storedDarkMode || preferDarkMode;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInittialDarkMode());
+  const [searchTerm, setSearchTerm] = useState("cat");
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
